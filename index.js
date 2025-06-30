0a3b65e6063ac89cb5e501f27437ff2a912fe47b
tariful6 <tariful2306@gmail.com>
2025-06-30 09:58:19 +0600

    jwt implement and add custom hook axios secure

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +10,31 @@ const port = process.env.PORT || 5000;
 //     origin :['http://localhost:5173/'],
 //     credentials : true,
 // }))
-app.use(cors())
+app.use(cors({
+  origin : ['http://localhost:5173'],
+  credentials : true
+}));
 app.use(express.json())
+app.use(cookieParser())
+
+
+// verify token middleware -------------------
+
+const verifyToken = (req, res, next)=> {
+    const token = req?.cookies?.token;
+    if(!token) return res.status(401).send({message : 'unauthorized access'})
+    if(token){
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,  decoded)=>{
+            if(err){
+                console.log(err);
+                return res.status(401).send({message : 'unauthorized access'})
+            }
+            console.log(decoded);
+            req.user = decoded;
+            next()
+            })
+        }
+}
 
 
 
@@ -32,6 +57,30 @@ async function run() {
 
     const jobCollection = client.db('soloSphere').collection('jobs')
     const bidCollection = client.db('soloSphere').collection('bids')
+
+//    Jwt related api ----------------------
+    app.post('/jwt', async(req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn : '1h'
+        })
+        res.cookie('token', token, {
+            httpOnly : true,
+            secure : false
+        }).send({success : true})
+    })
+
+
+    app.get('/logout', (req, res)=>{
+        res.clearCookie('token',{
+            httpOnly : true,
+            secure : false,
+            maxAge : 0,
+        }).send({success : true})
+    })
+
+
+    // job related api -----------------------------
 
     app.get('/jobs', async(req, res)=>{
         const result = await jobCollection.find().toArray();
@@ -59,8 +108,13 @@ async function run() {
     })
 
     
-    app.get('/job/:email', async(req, res)=>{
+    app.get('/job/:email',verifyToken, async(req, res)=>{
+        const tokenEmail = req.user.email;
+        // console.log(tokenEmail, 'from token ---');
         const email = req.params.email;
+        if(tokenEmail !== email){
+            return res.status(403).send({message : 'forbidden access'})
+        }
         const query = {'buyer.email' : email}
         const result = await jobCollection.find(query).toArray()
         res.send(result)
@@ -73,7 +127,7 @@ async function run() {
         res.send(result)
     })
 
-    app.put('/job/:id', async(req, res)=>{
+    app.put('/job/:id', verifyToken, async(req, res)=>{
         const id = req.params.id;
         const jobData = req.body;
         const query = {_id: new ObjectId(id)}
@@ -86,6 +140,41 @@ async function run() {
         const result = await jobCollection.updateOne(query, updateDoc, options)
         res.send(result)
     })
+
+
+    app.get('/myBids/:email',verifyToken, async(req, res)=>{
+        const email = req.params.email;
+        const query = {'email' : email}
+        const result = await bidCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    // app.get('/bidRequest/:email', async(req, res)=>{
+    //     const email = req.params.email;
+    //     const query = {'buyer.email' : email}
+    //     const result = await bidCollection.find(query).toArray();
+    //     res.send(result)
+    // })
+
+    app.get('/bidRequest/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      const query = {'buyer.email' : email}
+      const result = await bidCollection.find(query).toArray()
+      res.send(result)
+  })
+
+  app.patch('/bid/:id', async(req, res)=>{
+    const id = req.params.id;
+    const status = req.body;
+    const query = {_id: new ObjectId(id)}
+    const updateDoc ={
+        $set : status
+    }
+    const result = await bidCollection.updateOne(query, updateDoc);
+    res.send(result)
+
+
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
